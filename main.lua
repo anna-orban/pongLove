8fd268b3b0e018b66451fd7a00f6a67333bf2882
Anna Orban <anna.orban@yahoo.com>
2018-12-29 16:56:15 +0000

    ball is moving

--- main.lua
+++ main.lua
@@ -2,13 +2,35 @@ function love.load()
     ballX = love.graphics.getWidth() / 2
     ballY = love.graphics.getHeight() / 2
     ballRad = 5
-    ballSpeed = 100
+    ballSpeedX, ballSpeedY = love.math.random(100,200), love.math.random(100,200)
 end
 
 function love.update(dt)
-    ballX = ballX + ballSpeed * dt
+    ballX = ballX + ballSpeedX * dt
+    ballY = ballY + ballSpeedY * dt
+
+    if ballX < 0 then 
+        ballX = 0
+        ballSpeedX = -ballSpeedX
+    end
+
+    if ballX > love.graphics.getWidth() then
+        ballX = love.graphics.getWidth()
+        ballSpeedX = -ballSpeedX
+    end
+
+    if ballY < 0 then
+        ballY = 0
+        ballSpeedY = -ballSpeedY
+    end
+
+    if ballY > love.graphics.getHeight() then
+        ballY = love.graphics.getHeight()
+        ballSpeedY = -ballSpeedY
+    end 
 end
 
 function love.draw()
     love.graphics.circle('fill', ballX, ballY, ballRad)
+    love.graphics.print(string.format("%.0f", ballX), 10, 10)
 end
